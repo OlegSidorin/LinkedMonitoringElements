badbffe7f590e756c91871f438e86ecfca84d853
DDE3620-FQWKXS2@m1project.onmicrosoft.com <DDE3620-FQWKXS2@m1project.onmicrosoft.com>
2021-09-27 16:36:51 +0300

    Добавьте файлы проекта.

---
+++ LinkedMonitoringElements/Main.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.Attributes;
+using System;
+using Autodesk.Revit.DB.Events;
+
+namespace LinkedMonitoringElements
+{
+    [Transaction(TransactionMode.Manual), Regeneration(RegenerationOption.Manual)]
+    class Main : IExternalApplication
+    {
+        public static string DllLocation { get; set; }
+        public static string DllFolderLocation { get; set; }
+        public static string UserFolder { get; set; }
+        public static string TabName { get; set; } = "Надстройки";
+        public static string PanelName { get; set; } = "Мониторинг";
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            var techPanel = application.CreateRibbonPanel(PanelName);
+            DllLocation = Assembly.GetExecutingAssembly().Location;
+            DllFolderLocation = Path.GetDirectoryName(DllLocation);
+            UserFolder = @"C:\Users\" + Environment.UserName;
+            var MBtnData = new PushButtonData("MBtnData", "Zzzzzz\nzzzzzz", DllLocation, "LinkedMonitoringElements.MainCommand")
+            {
+                ToolTipImage = new BitmapImage(new Uri(Path.GetDirectoryName(DllLocation) + "\\res\\main.png", UriKind.Absolute)),
+                //ToolTipImage = PngImageSource("BatchAddingParameters.res.bap-icon.png"),
+                ToolTip = "--"
+            };
+            var TechBtn = techPanel.AddItem(MBtnData) as PushButton;
+            TechBtn.LargeImage = new BitmapImage(new Uri(Path.GetDirectoryName(DllLocation) + "\\res\\main.png", UriKind.Absolute));
+            //8TechBtn.LargeImage = PngImageSource("BatchAddingParameters.res.bap-icon.png");
+
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+    }
+}
---
+++ LinkedMonitoringElements/MainCommand.cs
@@ -0,0 +1,114 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using Application = Autodesk.Revit.ApplicationServices.Application;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+
+namespace LinkedMonitoringElements
+{
+    [Transaction(TransactionMode.Manual), Regeneration(RegenerationOption.Manual)]
+    class MainCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Application app = commandData.Application.Application;
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            MainCommandWindow mainCommandWindow = new MainCommandWindow();
+            // Get the element selection of current document.
+            //Autodesk.Revit.UI.Selection.Selection selection = uidoc.Selection;
+            ICollection<Autodesk.Revit.DB.ElementId> selectedIds = new Collection<ElementId>();
+            IList<Reference> references = uidoc.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element);
+            foreach (var reference in references)
+            {
+                selectedIds.Add(reference.ElementId);
+            };
+
+            //ICollection<Autodesk.Revit.DB.ElementId> selectedIds = uidoc.Selection.GetElementIds();
+            string str = "";
+            XYZ xyz = new XYZ();
+            string familyName = "";
+            Document linkedDocument = null;
+            if (selectedIds.Count > 0)
+            {
+                foreach (var eid in selectedIds)
+                {
+                    if (doc.GetElement(eid).IsMonitoringLinkElement())
+                    {
+                        var elem = doc.GetElement(eid) as FamilyInstance;
+                        LocationPoint lp = elem.Location as LocationPoint;
+                        xyz = lp.Point;
+                        familyName = elem.Name;
+                        IList<ElementId> eIds = elem.GetMonitoredLinkElementIds();
+                        foreach (var e in eIds)
+                        {
+                            RevitLinkInstance revitLinkInstance = doc.GetElement(e) as RevitLinkInstance;
+                            linkedDocument = revitLinkInstance.GetLinkDocument();
+                        }
+                        str += familyName + "\n" + linkedDocument.Title + "\n" + xyz.ToString();
+                    }  
+                }
+            }
+
+            //DocumentSet docSet = commandData.Application.Application.Documents;
+            //foreach(Document d in docSet)
+            //{
+            //    str += "\n" + d.Title;
+            //}
+
+            //IList<Element> elementsRVTLinks = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_RvtLinks).WhereElementIsNotElementType().ToElements();
+
+
+            //var ff = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_RvtLinks).WhereElementIsNotElementType().ToElements();
+
+            //foreach(var t in ff)
+            //{
+            //    str += t.Name;
+            //}
+            str += "\n------";
+            str += GetElementId_OfMonitoredElement(linkedDocument, familyName, xyz);
+            mainCommandWindow.textBlock_FamilyName.Text = str;
+            mainCommandWindow.Show();
+            //MessageBox.Show(str);
+            return Result.Succeeded;
+        }
+        ElementId GetElementId_OfMonitoredElement(Document linkedDoc, string FamilyName, XYZ xyz)
+        {
+            IList<Element> elements = new FilteredElementCollector(linkedDoc).OfClass(typeof(FamilyInstance)).WhereElementIsNotElementType().ToElements();
+            ElementId elementId = null;
+            List<FamilyInstance> fiList = new List<FamilyInstance>();
+            foreach (var element in elements)
+            {
+                var fi = element as FamilyInstance;
+                if (element.Name == FamilyName)
+                {
+                    fiList.Add(fi);
+                }
+            }
+            foreach (var fi in fiList)
+            {
+                LocationPoint lp = fi.Location as LocationPoint;
+                var point = lp.Point;
+                if (point.X == xyz.X)
+                {
+                    if (point.Y == xyz.Y)
+                    {
+                        if (point.Z == xyz.Z)
+                        {
+                            elementId = fi.Id;
+                        }
+                    }
+                }
+            }
+            return elementId;
+        }
+    }
+}
